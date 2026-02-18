90166470980c884661264c4acba9c19e1045dd1e
j-unisa <104635026+j-unisa@users.noreply.github.com>
2026-02-18 10:30:59 -0800

    docs: add game logic placeholders
    - outline nested array structure for 3x3 grid
    - define requirements for row and column input
    - document board rendering and separator logic
    - add placeholders for player class interaction

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,11 @@
 # TODO: Create separate file for player class
 # TODO: Add player choice as an instance variable
-# TODO: Add method to ask player for next choice
+# TODO: Add method to ask player for row choice
+# TODO: Add method to ask player for column choice
+# TODO: Hard code grid with any choices made and print it out when asking player for next choice
+# TODO: Create an array of 3 arrays representing the 3 rows of choices
+#       Each nested array will initially have 3 empty strings
+# TODO: Choices will be saved to the nested arrays
+# TODO: Use nested #each method to iterate through choices and print them out
+#       After each choice is printed, print a "|", unless it's the last choice
+#       After the outer #each method is done, print a new line with 5 dashes, unless it's the last array
